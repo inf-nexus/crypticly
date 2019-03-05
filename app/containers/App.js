2a7b9d4a1ef641c0adadb276ca3534ae07993907
Jacob Contreras <jacob6@vt.edu>
2019-03-04 22:46:53 -0500

    added custom JSS insertion point to allow for styled-component styles to take priority

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,15 +1,16 @@
 // @flow
-import * as React from 'react';
+import React, { Component, Fragment } from 'react';
+import type { Node } from 'react';
 
 type Props = {
-  children: React.Node
+  children: Node
 };
 
-export default class App extends React.Component<Props> {
+export default class App extends Component<Props> {
   props: Props;
 
   render() {
     const { children } = this.props;
-    return <React.Fragment>{children}</React.Fragment>;
+    return <Fragment>{children}</Fragment>;
   }
 }
--- app/containers/Root.js
+++ app/containers/Root.js
@@ -5,6 +5,20 @@ import { ConnectedRouter } from 'connected-react-router';
 import type { Store } from '../reducers/types';
 import Routes from '../Routes';
 
+import JssProvider from 'react-jss/lib/JssProvider';
+import { create } from 'jss';
+import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
+
+const styleNode = document.createComment('jss-insertion-point');
+document.head.insertBefore(styleNode, document.head.firstChild);
+
+const generateClassName = createGenerateClassName();
+const jss = create({
+  ...jssPreset(),
+  // We define a custom insertion point that JSS will look for injecting the styles in the DOM.
+  insertionPoint: 'jss-insertion-point'
+});
+
 type Props = {
   store: Store,
   history: {}
@@ -14,11 +28,13 @@ export default class Root extends Component<Props> {
   render() {
     const { store, history } = this.props;
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
+      <JssProvider jss={jss} generateClassName={generateClassName}>
+        <Provider store={store}>
+          <ConnectedRouter history={history}>
+            <Routes />
+          </ConnectedRouter>
+        </Provider>
+      </JssProvider>
     );
   }
 }
